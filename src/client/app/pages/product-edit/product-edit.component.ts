346aa135b985805043b7c9b7d643d879692e8647
filippomaria.galimberti.rossi@gmail.com <Filippo1618>
2018-06-06 06:50:45 +0000

    File generated by Skaffolder - user: filippomaria.galimberti.rossi@gmail.com

---
+++ src/client/app/pages/product-edit/product-edit.component.ts
@@ -0,0 +1,106 @@
+// Import Libraries
+import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+// Import Services
+import { ProductService } from '../../services/product.service';
+import { UserService } from '../../services/user.service';
+
+// Import Models
+import { Product } from '../../domain/ecommerce_db/product';
+
+import { User } from '../../domain/ecommerce_db/user';
+
+// START - USED SERVICES
+/*
+ *	productService.create
+ *		PARAMS: 
+ *		
+ *
+ *	UserService.findByuserProd
+ *		PARAMS: 
+ *					Objectid key - Id della risorsa userProd da cercare
+ *		
+ *
+ *	productService.get
+ *		PARAMS: 
+ *					ObjectId id - Id 
+ *		
+ *
+ *	productService.update
+ *		PARAMS: 
+ *					ObjectId id - Id
+ *		
+ *
+ */
+// END - USED SERVICES
+
+// START - REQUIRED RESOURCES
+/*
+ * UserService  
+ * productService  
+ */
+// END - REQUIRED RESOURCES
+
+/**
+ * Edit component for productEdit
+ */
+@Component({
+    selector: 'product-edit',
+    templateUrl : './product-edit.component.html',
+    styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent implements OnInit {
+
+    item: Product;
+    listUserProd: Product[];
+	externalUser: User[];
+    model: Product;
+    
+    constructor(
+        private productService: ProductService,
+        private userService: UserService,
+        private route: ActivatedRoute,
+        private location: Location) {
+        // Init item
+        this.item = new Product();
+	   this.externalUser = [];
+    }
+
+    ngOnInit(): void {
+            this.route.params.subscribe(param => {
+                let id: string = param['id'];
+                if (id !== 'new') {
+                    // Get item from server 
+                    this.productService.get(id).subscribe(item => this.item = item);
+                    
+                    
+                    this.userService.findByUserProd(id).subscribe(list => this.externalUser = list);
+                    
+                }
+            });
+    }
+
+    /**
+     * Save Item
+     */
+    save (formValid:boolean, item: Product): void{
+        if (formValid) {
+            if(item._id){
+                this.productService.update(item).subscribe(data => this.goBack());
+            } else {
+                this.productService.create(item).subscribe(data => this.goBack());
+            }  
+        }
+    }
+
+    /**
+     * Go Back
+     */
+    goBack(): void {
+        this.location.back();
+    }
+    
+
+}
---
+++ src/client/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,71 @@
+// Import Libraries
+import { Observable } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { ModalRemoveComponent } from '../../components/modal-remove.component';
+
+
+// Import Services
+import { ProductService } from '../../services/product.service';
+
+// Import Models
+import { Product } from '../../domain/ecommerce_db/product';
+
+import { User } from '../../domain/ecommerce_db/user';
+
+// START - USED SERVICES
+/*
+ *	productService.delete
+ *		PARAMS: 
+ *					ObjectId id - Id
+ *		
+ *
+ *	productService.list
+ *		PARAMS: 
+ *		
+ *
+ */
+// END - USED SERVICES
+
+// START - REQUIRED RESOURCES
+/*
+ * productService  
+ */
+// END - REQUIRED RESOURCES
+
+@Component({
+    selector: "product-list",
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    
+    // Attributes
+    list: Product[];
+    search: any = {};
+    idSelected: string;
+    
+    // Constructor
+    constructor(
+        private productService: ProductService, 
+        public dialog: MatDialog) {}
+
+    // Functions
+    ngOnInit(): void {
+        this.productService.list().subscribe(list => this.list = list);
+    }
+
+    openModal(id: string): void {
+        let dialogRef = this.dialog.open(ModalRemoveComponent, {
+            width: '250px',
+            data: () => {
+                // Execute on confirm
+                this.productService.remove(id).subscribe(() => {
+                    dialogRef.close();
+                });
+                this.list = this.list.filter(item => item._id != id);
+            }
+        });
+    }
+
+}
